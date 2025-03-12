3c0c961253584c9c37aaaf96bf73547bedd40f4c
mishaDev1 <buleshnyi.pn@ucu.edu.ua>
2025-03-12 11:12:34 +0200

    uncommented chunk of code for test

--- user.py
+++ user.py
@@ -1,8 +1,8 @@
 import requests
 
 # Post requests
-# for i in range(10):
-#     print("Recived status from facade-service post", requests.post("http://localhost:5000/send", json={"msg":f"message {i}"}))
+for i in range(10):
+    print("Recived status from facade-service post", requests.post("http://localhost:5000/send", json={"msg":f"message {i}"}))
 
 # Get request
 response = requests.get("http://localhost:5000/get")

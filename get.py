b79a7063518b145de64ccaa523bc288f6fb48cfd
mishaDev1 <buleshnyi.pn@ucu.edu.ua>
2025-04-12 22:36:29 +0300

    cleared code

--- get.py
+++
@@ -1,4 +0,0 @@
-import requests
-
-response = requests.get("http://localhost:5004/get")
-response = requests.get("http://localhost:5005/get")
--- user.py
+++ user.py
@@ -4,13 +4,11 @@ import requests
 for i in range(10):
     print("Recived status from facade-service post", requests.post("http://localhost:5000/send", json={"msg":f"message {i}"}))
 
-
 # Get request message service 
-
-# response = requests.get("http://localhost:5004/get")
-# response = requests.get("http://localhost:5005/get")
+response = requests.get("http://localhost:5004/get")
+response = requests.get("http://localhost:5005/get")
 
 # # Get request facade service
-# response = requests.get("http://localhost:5000/get")
-# print("Recived status from facade-service get", response)
-# print("Recieved data from facade-service get", response.json())
+response = requests.get("http://localhost:5000/get")
+print("Recived status from facade-service get", response)
+print("Recieved data from facade-service get", response.json())
